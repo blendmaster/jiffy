578e68d8e9e9e87268131414959902d0cdad3c48
Steven Ruppert <blendmaster@gmail.com>
2012-10-26 19:30:42 -0600

    Fully implement frame splitting. neat!

--- jiffy.js
+++ jiffy.js
@@ -5,6 +5,9 @@ function bits(byte, start, end) {
   return byte << (24 + start) >>> (32 + start - end)
 }
 function hex(a) { return map(a, function (n) { return n.toString(16) }).join() }
+function chars(a) {
+  return map(a, function (c) { return String.fromCharCode(c) }).join('')
+}
 
 function split() {
   var file
@@ -15,15 +18,13 @@ function split() {
     reader.onload = function () {
       window.bytes = this.result
       var data = Uint8Array(bytes)
-      if (!(map(data.subarray(0, 6), function(c) { return String.fromCharCode(c) }).join('')
-            === 'GIF89a')) {
+      if (!(chars(data.subarray(0, 6)) === 'GIF89a')) {
         return alert('not a gif, man!')
       }
       var width = Uint16Array(bytes, 6, 1)[0]
       var height = Uint16Array(bytes, 8, 1)[0]
 
       var colordata = data[10]
-      console.log(colordata)
       var gct            = bits(colordata, 0, 1)
       //, resolution     = bits(colordata, 1, 4)
       //, sort           = bits(colordata, 4, 5)
@@ -31,44 +32,49 @@ function split() {
 
       var endOfHeader = 13
                       + (gct === 1
-                        ? 3 * colorTableSize
+                        ? 3 * Math.pow(2, colorTableSize + 1)
                         : 0)
-      console.log(endOfHeader)
       var header = data.subarray(0, endOfHeader)
-      console.log(hex(header))
       var rest = data.subarray(endOfHeader)
-      console.log( hex(rest.subarray(0, 10)))
 
-      // eat app extension block
-      var i = 4
-      var bSize
-      while (bSize = rest[i]) { i += bSize }
-      i += 1 // skip terminator
+      // TODO detect actual GIF animation block "NETSCAPE2.0"
+      // skip app extension block
+      var imgdata = rest.subarray(19)
 
-      console.log(i)
-      var imgdata = rest.subarray(i)
+      var i = 0, start, blocks = [], lctf, tableSize, bSize
+      while (i < imgdata.length && imgdata[i] !== 0x3b) { // is not trailer
+        start = i
+        i += 8 // skip over graphic control extension block
+        i += 1
+        i += 8 // skip to packed byte
+        lctf      = bits(imgdata[i], 0, 1)
+        tableSize = bits(imgdata[i], 4, 8)
+        if (lctf) { i += (3 * Math.pow(2, tableSize + 1)) }
+        i += 1 // LZW minimum code size
+        i += 1 // to bsize
 
-      var j = 0, start, blocks = [], lctf, tableSize
-      while (j < imgdata.length && imgdata[j] !== 0x3b) { // is not trailer
-        start = j
-        j += 8 // skip over graphic control extension block
-        lctf = bits(imgdata[j + 8], 0, 1), tableSize = bits(imgdata[j + 8], 5, 8)
-        if (lctf) { j += 3 * tableSize }
-        j += 1 // LZW minimum code size
-        bSize = imgdata[j]
-        j += bSize
-        blocks.push(imgdata.subarray(start, j))
-        j += 1 //skip terminator
+        // eat up data blocks (size !== 0)
+        while (bSize = imgdata[i]) {
+          i += 1
+          i += bSize
+        }
+
+        blocks.push(imgdata.subarray(start, i))
+
+        i += 1 //skip terminator
       }
 
       // draw blocks
+      var frames = document.createElement('div')
+      frames.id = 'frames'
       blocks.forEach(function (b) {
         var blob = new Blob([header, b], {type: 'image/gif'})
         var url = URL.createObjectURL(blob)
         var i = new Image
         i.src = url
-        document.body.appendChild(i)
+        frames.appendChild(i)
       })
+      document.body.replaceChild(frames, document.getElementById('frames'))
     }
   }
 }
